a77a552385be13502c88f6e7abf406de6dbcf9be
vince <vince@10.3.81.108>
2026-04-22 15:36:14 -0400

    Adding things I did not know I needed to :)

---
+++ src/edu/kings/CommandEnum.java
@@ -0,0 +1,26 @@
+package edu.kings;
+
+public enum CommandEnum {
+	GO("go"),
+	QUIT("quit"),
+	HELP("help"),
+	LOOK("look"),
+	STATUS("status"),
+	BACK("back"),
+	TURNS("turns"),
+	SCORE("score"),
+	EXAMINE("examine"),
+	TAKE("take"),
+	DROP("drop"),
+	INVENTORY ("inventory");
+	
+	private final String word;
+	
+	private CommandEnum(String command) {
+		word= command;
+	}
+	
+	public String getCommand() {
+		return word;
+	}
+}
